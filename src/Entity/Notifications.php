db49e8f2c560280d77a9a7fa22df8c6840029fa6
CassandreLAMATY <lamatycassandre@gmx.fr>
2024-01-10 16:04:03 +0100

    subjects & notifications

---
+++ src/Entity/Notifications.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NotificationsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NotificationsRepository::class)]
+class Notifications
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $not_usr_id = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $not_type = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $not_content = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $not_crd_id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $not_sender_id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNotUsrId(): ?int
+    {
+        return $this->not_usr_id;
+    }
+
+    public function setNotUsrId(int $not_usr_id): static
+    {
+        $this->not_usr_id = $not_usr_id;
+
+        return $this;
+    }
+
+    public function getNotType(): ?string
+    {
+        return $this->not_type;
+    }
+
+    public function setNotType(string $not_type): static
+    {
+        $this->not_type = $not_type;
+
+        return $this;
+    }
+
+    public function getNotContent(): ?string
+    {
+        return $this->not_content;
+    }
+
+    public function setNotContent(string $not_content): static
+    {
+        $this->not_content = $not_content;
+
+        return $this;
+    }
+
+    public function getNotCrdId(): ?int
+    {
+        return $this->not_crd_id;
+    }
+
+    public function setNotCrdId(?int $not_crd_id): static
+    {
+        $this->not_crd_id = $not_crd_id;
+
+        return $this;
+    }
+
+    public function getNotSenderId(): ?int
+    {
+        return $this->not_sender_id;
+    }
+
+    public function setNotSenderId(?int $not_sender_id): static
+    {
+        $this->not_sender_id = $not_sender_id;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Subjects.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SubjectsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SubjectsRepository::class)]
+class Subjects
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $sbj_name = null;
+
+    #[ORM\Column(length: 9)]
+    private ?string $sbj_color = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSbjName(): ?string
+    {
+        return $this->sbj_name;
+    }
+
+    public function setSbjName(string $sbj_name): static
+    {
+        $this->sbj_name = $sbj_name;
+
+        return $this;
+    }
+
+    public function getSbjColor(): ?string
+    {
+        return $this->sbj_color;
+    }
+
+    public function setSbjColor(string $sbj_color): static
+    {
+        $this->sbj_color = $sbj_color;
+
+        return $this;
+    }
+}
