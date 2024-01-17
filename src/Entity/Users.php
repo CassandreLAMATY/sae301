49d33df9488434b1f128817594838690400501ad
CassandreLAMATY <lamatycassandre@gmx.fr>
2024-01-17 23:52:10 +0100

    remove duplicates

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -42,6 +42,21 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 2)]
     private ?string $usr_semester = null;
 
+    #[ORM\Column]
+    private ?bool $usr_homework_reminder = false;
+
+    #[ORM\Column]
+    private ?bool $usr_exam_reminder = false;
+
+    #[ORM\Column]
+    private ?bool $usr_new_reminder = false;
+
+    #[ORM\Column]
+    private ?bool $usr_modif_reminder = false;
+
+    #[ORM\Column]
+    private ?bool $usr_cookies = false;
+
     public function getUsrId(): ?int
     {
         return $this->usr_id;
@@ -151,20 +166,20 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
 
-    return [$this->getUsrRole()];
+        return [$this->getUsrRole()];
     }
 
     public function eraseCredentials(): void
     {
-            // TODO: Implement eraseCredentials() method.
+        // TODO: Implement eraseCredentials() method.
     }
 
     public function getUserIdentifier(): string
     {
-        return $this->getUsrPseudo();//changer ici si on veut se connecter avec le pseudo
+        return $this->getUsrPseudo(); //changer ici si on veut se connecter avec le pseudo
 
     }
-    
+
     public function getUsrSemester(): ?string
     {
         return $this->usr_semester;
@@ -173,6 +188,66 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
     public function setUsrSemester(string $usr_semester): static
     {
         $this->usr_semester = $usr_semester;
+
+        return $this;
+    }
+
+    public function isUsrHomeworkReminder(): ?bool
+    {
+        return $this->usr_homework_reminder;
+    }
+
+    public function setUsrHomeworkReminder(bool $usr_homework_reminder): static
+    {
+        $this->usr_homework_reminder = $usr_homework_reminder;
+
+        return $this;
+    }
+
+    public function isUsrExamReminder(): ?bool
+    {
+        return $this->usr_exam_reminder;
+    }
+
+    public function setUsrExamReminder(bool $usr_exam_reminder): static
+    {
+        $this->usr_exam_reminder = $usr_exam_reminder;
+
+        return $this;
+    }
+
+    public function isUsrNewReminder(): ?bool
+    {
+        return $this->usr_new_reminder;
+    }
+
+    public function setUsrNewReminder(bool $usr_new_reminder): static
+    {
+        $this->usr_new_reminder = $usr_new_reminder;
+
+        return $this;
+    }
+
+    public function isUsrModifReminder(): ?bool
+    {
+        return $this->usr_modif_reminder;
+    }
+
+    public function setUsrModifReminder(bool $usr_modif_reminder): static
+    {
+        $this->usr_modif_reminder = $usr_modif_reminder;
+
+        return $this;
+    }
+
+    public function isUsrCookies(): ?bool
+    {
+        return $this->usr_cookies;
+    }
+
+    public function setUsrCookies(bool $usr_cookies): static
+    {
+        $this->usr_cookies = $usr_cookies;
 
         return $this;
     }
